ed2bc2e1a8e07ce59cb8cf43f81074506fb088e4
Play Easy Now <16202860+playeasynow@users.noreply.github.com>
2018-09-28 14:42:49 -0500

    added final JS

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,84 +1,79 @@
-// Initialize Firebase
-var config = {
-    apiKey: "AIzaSyAicv50o9XdooA0mA4DI0hRPLuMnlJyRws",
-    authDomain: "train-activity-49b28.firebaseapp.com",
-    databaseURL: "https://train-activity-49b28.firebaseio.com",
-    projectId: "train-activity-49b28",
-    storageBucket: "train-activity-49b28.appspot.com",
-    messagingSenderId: "52372351647"
-  };
-  firebase.initializeApp(config);
-
-// Get a reference to the database service
-var database = firebase.database();
-
-var trainNumber = 0;
-
-// On click event associated with the add train information
-$("#add-train").on("click", function (event) {
-    // prevent form from trying to submit/refresh the page
-    event.preventDefault();
-
-    var trainScheduler = {
-        name: '',
-        destination: '',
-        time: '',
-        frequency: '',
-        minutes: '',
-        dateAdded: firebase.database.ServerValue.TIMESTAMP
+$(document).ready(function () {
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyAicv50o9XdooA0mA4DI0hRPLuMnlJyRws",
+        authDomain: "train-activity-49b28.firebaseapp.com",
+        databaseURL: "https://train-activity-49b28.firebaseio.com",
+        projectId: "train-activity-49b28",
+        storageBucket: "train-activity-49b28.appspot.com",
+        messagingSenderId: "52372351647"
     };
 
-    trainNumber++;
+    firebase.initializeApp(config);
 
-    // Capture User Inputs and store them into variables
-    trainScheduler.name = $("#train-time").val().trim();
-    trainScheduler.destination = $("#destination").val().trim();
-    trainScheduler.time = $("#time").val().trim();
-    trainScheduler.frequency = $("#frequency").val().trim();
-    // trainScheduler.minutes = $("#minutes-away-display").val().trim();
+    // reference to the database service
+    var database = firebase.database();
 
-    var newRow = $("<tr>");
+    // on click event associated with added train information
+    $("#add-train").on("click", function (event) {
+        // prevent form from trying to submit/refresh the page
+        event.preventDefault();
 
-    var newTrainRow = $("<td class='name-display'>" + trainNumber).text(trainScheduler.name);
-    var newDestinationRow = $("<td class='destination-display'>" + trainNumber).text(trainScheduler.destination);
-    var newTimeRow = $("<td class='train-time-display'>" + trainNumber).text(trainScheduler.time);
-    var newFrequencyRow = $("<td class='frequency-display'>" + trainNumber).text(trainScheduler.frequency);
-    // var newMinutesAwayRow = $("<td class='minutes-away-display'>").text(trainScheduler.minutes);
+        var train = {
+            name: '',
+            destination: '',
+            frequency: '',
+            firstTrain: '',
+            dateAdded: firebase.database.ServerValue.TIMESTAMP
+        };
 
-    newRow.append(newTrainRow, newDestinationRow, newTimeRow, newFrequencyRow);
+        // Capture User Inputs and store them into variables
+        train.name = $("#train-time").val().trim();
+        train.destination = $("#destination").val().trim();
+        train.frequency = $("#frequency").val().trim();
+        train.firstTrain = $("#time").val().trim();
 
-    $("tbody").append(newRow);
+        database.ref().push(train);
 
-    database.ref().push(trainScheduler);
+        $("#train-time").val("");
+        $("#destination").val("");
+        $("#frequency").val("");
+        $("#time").val("");
 
-});
+    });
 
-// Firebase is always watching for changes to the data.
-// When changes occurs it will print them to console and html
-database.ref().on("value", function (snapshot) {
+    database.ref().on("child_added", function (snapshot, prevChildKey) {
 
-    // Change the HTML
-    $("#name-display").text(snapshot.val().name);
-    $("#destination-display").text(snapshot.val().destination);
-    $("#train-time-display").text(snapshot.val().time);
-    $("#frequency-display").text(snapshot.val().frequency);
-    $("#minutes-away-display").text(snapshot.val().minutes);
-}, function (errorObject) {
-    console.log("The read failed: " + errorObject.code);
-});
+        var newTrain = snapshot.val();
 
-database.ref().on("child_added", function (snapshot, prevChildKey) {
+        var tableRow = $('<tr>');
+        var tdName = $('<td>').text(newTrain.name);
+        var tdDestination = $('<td>').text(newTrain.destination);
 
-    var newPost = snapshot.val();
 
-    // Change the HTML
-    $(".name-display" + trainNumber).text(newPost.name);
-    $(".destination-display" + trainNumber).text(newPost.destination);
-    $(".train-time-display"+ trainNumber).text(newPost.time);
-    $(".frequency-display" + trainNumber).text(newPost.frequency);
-    // var newMins= $("#minutes-away-display").text(newPost.minutes);
+        var frequency = newTrain.frequency;
+        var firstTrain = newTrain.firstTrain;
 
-    // $("#tableRows").append(newRow);
-}, function (errorObject) {
-    console.log("The read failed: " + errorObject.code);
+        var currentTime = moment();
+
+        // subtracts the first train time back a year to ensure it's before current time.
+        var firstTrainConverted = moment(firstTrain, "hh:mm").subtract("1, years");
+        // the time difference between current time and the first train
+        var difference = currentTime.diff(moment(firstTrainConverted), "minutes");
+        var remainder = difference % frequency;
+        var minUntilTrain = frequency - remainder;
+        var nextTrain = currentTime.add(minUntilTrain, "minutes").format("hh:mm a");
+
+        var tdNextArrival = $('<td>').text(nextTrain);
+        var tdMinutes = $('<td>').text(minUntilTrain);
+
+        var tdFrequency = $('<td>').text(frequency);
+
+        tableRow.append(tdName, tdDestination, tdFrequency, tdNextArrival, tdMinutes);
+        $('#tableData').append(tableRow);
+
+    }, function (errorObject) {
+        console.log("The read failed: " + errorObject.code);
+    });
+
 });
